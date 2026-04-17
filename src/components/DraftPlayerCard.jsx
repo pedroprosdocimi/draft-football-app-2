236310f27cf2592cd122eeb3312ced0b184557c2
Pedro <pedroprosdocimi@gmail.com>
2026-04-16 21:52:21 -0300

    style: tweak draft matchup crest placement

--- src/components/DraftPlayerCard.jsx
+++ src/components/DraftPlayerCard.jsx
@@ -320,7 +320,7 @@ export default function DraftPlayerCard({ player, onClick, isMyTurn, compact = f
           <div
             style={{
               position: 'absolute',
-              left: compact ? 5 : 7,
+              left: compact ? 3 : 4,
               bottom: compact ? 26 : 34,
               display: 'flex',
               alignItems: 'center',
@@ -352,8 +352,8 @@ export default function DraftPlayerCard({ player, onClick, isMyTurn, compact = f
                   src={logoURL}
                   alt=""
                   style={{
-                    width: compact ? 14 : large ? 22 : 18,
-                    height: compact ? 14 : large ? 22 : 18,
+                    width: compact ? 15 : large ? 24 : 19,
+                    height: compact ? 15 : large ? 24 : 19,
                     objectFit: 'contain',
                     filter: 'drop-shadow(0 2px 5px rgba(0,0,0,0.45))',
                   }}
